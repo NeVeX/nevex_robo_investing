a8a4537f8a0e7916bc3df819202d2943ca36b59e
NeVeX <markcunningham9@gmail.com>
2017-08-07 23:40:55 -0700

    Creating the application skeleton

---
+++ src/main/java/com/nevex/roboinvesting/RoboInvestingApplication.java
@@ -0,0 +1,16 @@
+package com.nevex.roboinvesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Mark Cunningham on 8/7/2017.
+ */
+@SpringBootApplication
+public class RoboInvestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoboInvestingApplication.class, args);
+    }
+
+}
